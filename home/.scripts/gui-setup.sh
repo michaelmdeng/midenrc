9204ea0b6ec3156f4a3eb9d614d65a3cd9743754
Michael Deng <michaelmdeng@gmail.com>
2022-02-28 22:40:02 -0800

    Spelling

---
+++ home/.scripts/gui-setup.sh
@@ -0,0 +1,16 @@
+# Basic GUI
+sudo apt install baobab gnome-tweak-tool
+
+# Dropbox
+sudo apt install libatomic1
+cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86" | tar xzf -
+
+# Keepass
+sudo apt install xdotool keepass2
+
+# touchpad gestures
+sudo apt install -y xdotool libinput-tools
+sudo gem install fusuma
+
+# CopyQ clipboard manager
+sudo apt install copyq
