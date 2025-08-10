7b7c6b3dbaa61888abe943a418f5ba631beba64b
Michael Deng <michaelmdeng@gmail.com>
2025-08-10 14:56:15 -0700

    feat: add directory picker for live grep in telescope

--- home/.config/nvim/lua/md-telescope.lua
+++ home/.config/nvim/lua/md-telescope.lua
@@ -1,3 +1,8 @@
+local builtin = require('telescope.builtin')
+local actions = require('telescope.actions')
+local action_state = require('telescope.actions.state')
+local action_layout = require('telescope.actions.layout')
+
 vim.api.nvim_set_keymap('n', '<C-p>', 'HasGit() ? "<cmd>Telescope git_files<cr>" : "<cmd>Telescope find_files<cr>"', { expr = true, noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>p', '<cmd>lua require"telescope.builtin".find_files()<cr>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader><C-p>', '<cmd>Telescope oldfiles<cr>', { noremap = true, silent = true })
@@ -12,15 +17,33 @@ vim.api.nvim_set_keymap('n', '<leader>H', '<cmd>Telescope git_bcommits<cr>', { n
 vim.api.nvim_set_keymap('n', '<leader>"', '<cmd>Telescope registers<cr>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>z', '<cmd>Telescope spell_suggest<cr>', { noremap = true, silent = true })
 
+local function pick_dir_and_grep()
+  builtin.find_files({
+    prompt_title = "Pick search directory",
+    cwd = vim.loop.cwd(),
+    find_command = { "fd", "--type", "d" },
+    attach_mappings = function(prompt_bufnr, map)
+      local function on_select()
+        local entry = action_state.get_selected_entry()
+        actions.close(prompt_bufnr)
+        if entry and entry[1] then
+          builtin.live_grep({ search_dirs = { entry[1] } })
+        end
+      end
+      map("i", "<CR>", on_select)
+      map("n", "<CR>", on_select)
+      return true
+    end,
+  })
+end
+vim.keymap.set('n', '<leader><C-f>', pick_dir_and_grep, { desc = "Live grep in chosen directory" })
+
 -- LSP
 vim.api.nvim_set_keymap('n', '<leader>im', '<cmd>Telescope lsp_implementations<cr>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>df', '<cmd>Telescope lsp_definitions<cr>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>sy', '<cmd>Telescope lsp_document_symbols<cr>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>rf', '<cmd>Telescope lsp_references<cr>', { noremap = true, silent = true })
 
-
-local actions = require('telescope.actions')
-local action_layout = require('telescope.actions.layout')
 require('telescope').setup{
   defaults = {
     layout_strategy = 'vertical',
